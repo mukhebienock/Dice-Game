19b5af5b7b819216753ef4f3358b6816c66ac0d3
Enock Mukhebi <mukhebienock@gmail.com>
2017-06-18 19:11:23 +0300

    modify the random number generator

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,17 @@
 $(document).ready(function() {
 
     function printNumber(number) {
-      var placeholder = document.getElementById('placeholder');
+      var placeholder = document.getElementById('placeholder')
       placeholder.innerHTML = number;
     }
+
 
     var button = document.getElementById('button');
 
     button.onclick = function() {
-      var result = dice.roll();
-      printNumber(result);
+      var outcome = dice.roll();
+      printNumber(outcome);
+
     };
 
 
